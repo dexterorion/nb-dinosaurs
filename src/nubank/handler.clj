ff4624ac11b2624a9cb2b9730c9d0358f4a41a6b
Vinicius Nordi <viniciusnordi@Nagibs-MacBook-Pro.local>
2019-05-24 07:38:14 -0300

    Adding grid methods

--- src/nubank/handler.clj
+++ src/nubank/handler.clj
@@ -1,11 +1,29 @@
 (ns nubank.handler
   (:require [compojure.core :refer :all]
             [compojure.route :as route]
-            [ring.middleware.defaults :refer [wrap-defaults site-defaults]]))
+            [compojure.handler :as handler]
+            [ring.middleware.json :as json]
+            [ring.util.response :refer [response]]))
+
+(def grid (atom (vec (replicate 250 0))))
+
+(defn robot [request]
+  (response {:robot "bip"}))
+
+(defn space [request]
+  (response {:space "uon"}))
+
+(defn matrix [request]
+  (assoc grid 1 5)
+  (response {:value (get grid 1 100)}))
 
 (defroutes app-routes
-  (GET "/" [] "Hello World")
+  (GET "/robot" [] robot)
+  (GET "/space" [] space)
+  (GET "/matrix" [] matrix)
   (route/not-found "Not Found"))
 
-(def app
-  (wrap-defaults app-routes site-defaults))
+(def app 
+  (-> (handler/api app-routes)
+      (json/wrap-json-params)
+      (json/wrap-json-response)))
