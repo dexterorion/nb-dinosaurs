4c475ad834381689239322177f2ac8b5e34521ad
Vinicius Nordi <viniciusnordi@Nagibs-MacBook-Pro.local>
2019-05-26 16:21:09 -0300

    Creating robots and dinosaurs and storing on memory

--- src/nubank/handler.clj
+++ src/nubank/handler.clj
@@ -5,25 +5,73 @@
             [ring.middleware.json :as json]
             [ring.util.response :refer [response]]))
 
-(def grid (atom (vec (replicate 250 0))))
+(def tbsize (atom 50))
 
-(defn robot [request]
-  (response {:robot "bip"}))
+(def dinosaurs (atom {}))
 
-(defn space [request]
-  (response {:space "uon"}))
+(def robots (atom {}))
 
-(defn matrix [request]
-  (assoc grid 1 5)
-  (response {:value (get grid 1 100)}))
+; (def find-x [request]
+;   (print (get-in request[:body]))
+;   (+ (/ pos @tbsize) 1))
+
+; (def find-y [request]
+;   (print (get-in request[:body]))
+;   (rem pos @tbsize) 1)
+
+; (def find-pos [request]
+;   (print (get-in request[:body]))
+;   (+ (* (- x 1) @tbsize) y))
+
+(defn onlypost [req]
+    (print "aaa"))
+
+(def grid (atom nil))
+
+; (defn robot [request]
+;   (response {:robot "bip"}))
+
+; (defn matrix [request]
+;   (swap! grid assoc 1 5)
+;   (response {:value (get @grid 1)}))
+
+; (defn reset-space [request] 
+;   (reset! grid (vec (replicate 250 0)))
+;   (response (apply str @grid)))
+
+; (defn show-space [request] 
+;   (response (apply str @grid)))
+
+(defn uuid [] (.toString (java.util.UUID/randomUUID)))
+
+(defn create-dinosaur [request] (
+      let [position (or (get-in request [:body :position]) 1)
+          id (uuid)]
+      (swap! dinosaurs assoc id {:position position})
+      (response @dinosaurs)))
+
+(defn create-robot [request] (
+      let [position (or (get-in request [:body :position]) 1)
+          id (uuid)
+          direction (or (get-in request [:body :direction]) 1)]
+      (swap! robots assoc id {:position position :direction direction})
+      (response @robots)))
 
 (defroutes app-routes
-  (GET "/robot" [] robot)
-  (GET "/space" [] space)
-  (GET "/matrix" [] matrix)
+  (POST "/dinosaur" request create-dinosaur)
+  (POST "/robot" request create-robot)
+  (POST "/" request
+    (let [name (or (get-in request [:params :name])
+                   (get-in request [:body :name])
+                   "John Doe")]
+      {:status 200
+       :body {:name name
+       :desc (str "The name you sent to me was " name)}}))
+  (route/resources "/")
   (route/not-found "Not Found"))
 
 (def app 
   (-> (handler/api app-routes)
+      (json/wrap-json-body {:keywords? true})
       (json/wrap-json-params)
       (json/wrap-json-response)))
